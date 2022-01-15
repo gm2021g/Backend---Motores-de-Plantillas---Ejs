65edd73c98dfe892e2252fcffc80cc35eb059796
MariaIsabelGonzalez <gm2021g@gmail.com>
2022-01-15 02:07:05 -0300

    Desafío plantillas Ejs

---
+++ index.js
@@ -0,0 +1,31 @@
+const path = require('path');
+const express = require('express');
+const { productos } = require('./data/data');
+
+const rutasApi = require('./routers/app.router');
+
+const app = express();
+const PORT = process.env.PORT || 8080;
+
+// Middlewares
+app.use(express.static(path.resolve(__dirname, './public')));
+
+//Templatess Engine
+app.set('view engine', 'ejs');
+app.set('views', './views');
+
+// Rutas
+app.use('/api', rutasApi);
+app.get('/productos', (req, res) => {
+  res.render('ejs/index', { showAllProducts: true, products: productos })
+})
+
+const connectedServer = app.listen(PORT, () => {
+  console.log(`Servidor activo y escuchando en el puerto ${PORT}`);
+});
+
+connectedServer.on('error', (error) => {
+  console.log(error.message);
+})
+
+
